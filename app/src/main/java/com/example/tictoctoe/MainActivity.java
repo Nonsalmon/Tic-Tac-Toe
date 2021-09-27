3c2c929730d65d4af9893f092a06a342a719a937
Nonsalmon <fxred727@gmail.com>
2021-09-27 23:19:48 +0700

    Tic-Tac_Toe App

--- app/src/main/java/com/example/tictoctoe/MainActivity.java
+++ app/src/main/java/com/example/tictoctoe/MainActivity.java
@@ -58,14 +58,9 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     }
 
     private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -74,8 +69,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
-    // Shows the system bars by removing all the flags
-// except for the ones that make the content appear under the system bars.
+
     private void showSystemUI() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
